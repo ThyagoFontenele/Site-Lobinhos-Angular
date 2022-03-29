7065375962a06a5952c00b9ef6e9a3d85f962f3b
Thyago Fontenele <thyago.fontenele@injunior.com.br>
2022-03-29 10:55:04 -0300

    menu burger funcionando e header scrolleffect tbm

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  class: string = 'menu-burger'
+  navClass: string = 'nav';
+  classHeader: string = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener("document:scroll")
+  scrollFunction(){
+    if(document.documentElement.scrollTop > 700){
+      this.classHeader = 'active';
+    }
+    else{
+      this.classHeader = ''
+    }
+  }
+
+  toggleActive(){
+    if(this.class === 'menu-burger'){
+      this.class = 'menu-burger active';
+      this.navClass = 'nav active';
+    }
+    else{ 
+      this.class = 'menu-burger';
+      this.navClass = 'nav';
+    }
+  }
 }
